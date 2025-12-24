99b75f643bbf54ccc4caf0fc4479a0ea689ca062
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-12-24 18:06:13 -0300

    Ajustando o host da conexão com o banco

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $pdo = null;
 
 try {
-    $pdo = new PDO('mysql:host=mysql;dbname=exemplo;', 'php', '123456');
+    $pdo = new PDO('mysql:host=localhost;dbname=exemplo;', 'php', '123456');
 } catch (Exception $e) {
     echo $e->getMessage();
     die();
