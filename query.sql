d536cfaec626b984654f462c0f831faabc2b399d
Marcos Aleixo <maknytz@gmail.com>
2025-12-25 13:30:45 -0300

    update queries

--- query.sql
+++ query.sql
@@ -1,8 +1,10 @@
 create database exemplo;
 use exemplo;
 create table produtos(
-	id int(11) not null auto_increment,
+	id int not null auto_increment,
     descricao varchar(50) default null,
     primary key(id)
 );
 describe produtos;
+select * from produtos;
+alter table produtos auto_increment = 2;
